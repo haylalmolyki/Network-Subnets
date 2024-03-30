1c4490e105a39b7de2da685fe7d103bb91deb700
hayl almolyki <haylalmolyki@gmail.com>
2024-03-31 01:29:01 +0300

    subnet

--- subnet.py
+++ subnet.py
@@ -2,61 +2,61 @@
 uniquecode = 22528
 
 #problem 1
-p1Network = '0.147.126.92'
+p1Network = '0.147.126.64'
 p1Broadcast = '0.147.126.127'
 p1First = '0.147.126.65'
 p1Last =  '0.147.126.126'
 
 #problem 2
-p2Network = '167.132.88.134'
+p2Network = '167.132.88.0'
 p2Broadcast = '167.132.88.255'
 p2First = '167.132.88.1'
 p2Last = '167.132.88.254'
 
 #problem 3
-p3Network = '78.117.49.177'
+p3Network = '78.117.49.160'
 p3Broadcast = '78.117.49.191'
 p3First = '78.117.49.161'
 p3Last = '78.117.49.190'
 
 #problem 4
-p4Network = '245.102.11.219'
+p4Network = '245.102.11.128'
 p4Broadcast = '245.102.11.255'
 p4First = '245.102.11.129'
 p4Last = '245.102.11.254'
 
 #problem 5
-p5Network = '156.87.229.6'
+p5Network = '156.87.229.0'
 p5Broadcast = '156.87.229.15'
 p5First = '156.87.229.1'
 p5Last = '156.87.229.14'
 
 #problem 6
-p6Network = '67.72.191.48'
+p6Network = '67.72.191.0'
 p6Broadcast = '67.72.191.63'
 p6First = '67.72.191.1'
 p6Last =  '67.72.191.62'
 
 #problem 7
-p7Network = '234.58.152.91'
+p7Network = '234.58.152.88'
 p7Broadcast = '234.58.152.95'
 p7First = '234.58.152.89'
 p7Last = '234.58.152.94'
 
 #problem 8
-p8Network = '145.43.114.133'
+p8Network = '145.43.114.128'
 p8Broadcast = '145.43.114.159'
 p8First = '145.43.114.129'
 p8Last = '145.43.114.158'
 
 #problem 9
-p9Network = '56.28.76.176'
+p9Network = '56.28.76.0'
 p9Broadcast = '56.28.76.255'
 p9First = '56.28.76.1'
 p9Last = '56.28.76.254'
 
 #problem 10
-p10Network = '223.13.38.218'
+p10Network = '223.13.38.208'
 p10Broadcast = '223.13.38.223'
 p10First = '223.13.38.209'
 p10Last = '223.13.38.222'
